d5b26a3ae8f2e705fbbe3ae5a059b137f718eda0
Tursunov Muhammadsayyid <114228094+SayyeedDeveloper@users.noreply.github.com>
2024-11-06 06:23:20 +0000

    finished ex6

--- ex6/main.c
+++ ex6/main.c
@@ -1,7 +1,28 @@
-
 #include <stdio.h>
 
-int main(void)
-{
-    printf("Hello World\n");
+int main() {
+    int x, y, M, m, r;
+    
+    printf("Enter two numbers:\n");
+    scanf("%d %d", &x, &y);
+    
+    if (x > y) {
+        M = x;
+        m = y;
+    } else {
+        M = y;
+        m = x;
+    }
+    
+    while (1) {
+        r = M % m;
+        if (r == 0) {
+            break;
+        }
+        M = m;
+        m = r;
+    }
+    
+    printf("GCD is: %d\n", m);
+    return 0;
 }

c1a013d185a2e938114a386f5614dadda1eb76f7
Tursunov Muhammadsayyid <114228094+SayyeedDeveloper@users.noreply.github.com>
2024-11-06 06:43:00 +0000

    finished ex7

--- ex7/main.c
+++ ex7/main.c
@@ -1,7 +1,35 @@
-
 #include <stdio.h>
 
-int main(void)
-{
-    printf("Hello World\n");
+int main() {
+    int n;
+    int ascending = 1;
+    int descending = 1;
+    int prev, curr;
+    
+    printf("Enter n: ");
+    scanf("%d", &n);
+    
+    printf("Enter first number: ");
+    scanf("%d", &prev);
+
+    for (int i = 1; i < n; i++) {
+        printf("Enter next number: ");
+        scanf("%d", &curr);
+        
+        if (curr <= prev)
+            ascending = 0;
+        if (curr >= prev)
+            descending = 0;
+            
+        prev = curr;
+    }
+    
+    if (ascending)
+        printf("ascending sequence\n");
+    else if (descending)
+        printf("descending sequence\n");
+    else
+        printf("neither ascending nor descending sequence\n");
+    
+    return 0;
 }
